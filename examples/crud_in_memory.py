a4e2171f6dcbe66c6772bad8cf6e425b085753a3
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2026-07-04 00:30:48 +0800

    Add atomic writebatch (not thread-safe)

---
+++ examples/crud_in_memory.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from cailloudb import ObjectStore, DbBuilder
+
+
+async def main():
+    store = ObjectStore.resolve(":memory:")
+    builder = DbBuilder("", store)
+    db = builder.build()
+
+    await db.put(b"entry1", b"value1")
+    await db.put(b"entry2", b"value2")
+
+    print(await db.get(b"entry1"))
+    print(await db.get(b"entry2"))
+
+
+asyncio.run(main())
--- src/cailloudb/db.py
+++ src/cailloudb/db.py
@@ -1,4 +1,8 @@
-from store import Store
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from store import Store
+    from write_batch import WriteBatch
 
 
 class Db:
@@ -10,8 +14,8 @@ class Db:
     async def get(self, key: bytes):
         return await self.store.get(key)
 
-    async def put(self, key: bytes, value: bytes):
-        await self.store.put(key, value)
+    async def put(self, key: bytes, val: bytes):
+        await self.store.put(key, val)
 
     async def delete(self, key: bytes):
         await self.store.delete(key)
@@ -20,3 +24,10 @@ class Db:
         return await self.store.exists(key)
 
     async def shutdown(self): ...
+
+    async def write(self, batch: WriteBatch):
+        for key, val in batch:
+            if val:
+                await self.put(key, val)
+            else:
+                await self.delete(key)
